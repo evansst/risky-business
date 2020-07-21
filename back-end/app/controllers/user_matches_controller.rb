66d67f4f7f92403d06d36a8358b1cae632168d45
Sam Evans <evansst@icloud.com>
2020-07-21 13:05:17 -0600

    Adds user match controller methods

--- back-end/app/controllers/user_matches_controller.rb
+++ back-end/app/controllers/user_matches_controller.rb
@@ -1,2 +1,29 @@
 class UserMatchesController < ApplicationController
+  def index
+    @user_matches = UserMatch.all
+
+    render json: @user_matches
+  end
+
+  def show
+    @user_match = UserMatch.find(params[:id])
+
+    render json: @user_match
+  end
+
+  def create
+    @user_match = UserMatch.create(
+      user_id: params[:user_id],
+      match_id: params[:match_id]
+    )
+
+    render json: @user_match
+  end
+
+  def destroy
+    @user_match = UserMatch.find(params[:id])
+    @user_match.destroy
+
+    render json: 'Match deleted'
+  end
 end
