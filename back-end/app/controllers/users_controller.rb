f4e30f818d4bf2e8fcaf8d78219d8972ecc7ef6b
bgordon8 <gordonbrian16@gmail.com>
2020-07-21 12:35:50 -0600

    fixed update, create, and destroy controller actions

--- back-end/app/controllers/users_controller.rb
+++ back-end/app/controllers/users_controller.rb
@@ -26,19 +26,22 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.find(params [:id])
-        @user.update(username: params[:username],
+        @user = User.find(params[:id])
+        @user.update(
+        username: params[:username],
         password: params[:password],
         email: params[:email],
         name: params[:name],
         interests: params[:interests],
-        city: params[:city])
+        city: params[:city]
+        )
+    
 
         render json: "Profile has been updated!"
     end
 
     def destroy
-        @user = User.find(params [:id])
+        @user = User.find(params[:id])
         @user.destroy
 
         render json: "Profile deleted"
